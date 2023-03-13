a41f1b55b4d953e62ac364b90d14dd22a10ddd8a
Masaki Matsumura <mm.schwarz@gmail.com>
2023-03-13 14:38:02 +0900

    remove unnecessary expo-web-browser

--- App.tsx
+++ App.tsx
@@ -8,8 +8,6 @@ import {
   StackNavigationProp,
 } from '@react-navigation/stack'
 
-import { maybeCompleteAuthSession } from 'expo-web-browser'
-import { makeRedirectUri } from 'expo-auth-session'
 import * as Google from 'expo-auth-session/providers/google'
 import Constants from 'expo-constants'
 
@@ -30,8 +28,6 @@ export type TopScreenNavigationProps = StackNavigationProp<
   RootStackParamList,
   'Login'
 >
-
-maybeCompleteAuthSession()
 
 const App = () => {
   const [userLogged, setUserLogged] = useState(false)
@@ -64,11 +60,9 @@ const App = () => {
   )
   const AppStack = createStackNavigator()
   const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: Constants.expoConfig?.extra?.webClientId,
-    scopes: ['profile', 'email'],
-    redirectUri: makeRedirectUri({
-      scheme: 'https://auth.expo.io/@masakichi/nature-log/start',
-    }),
+    expoClientId: Constants.expoConfig?.extra?.webClientId,
+    iosClientId: Constants.expoConfig?.extra?.iosClientId,
+    androidClientId: Constants.expoConfig?.extra?.androidClientId,
   })
 
   useEffect(() => {
